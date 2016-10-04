aa520b43e245ab2232d2dccfbedc1910be5e1916
Leonardo Neto <leonardoaquinoneto@gmail.com>
2016-10-04 10:28:31 -0400

    Commit inicial
    Apenas teste

---
+++ src/App.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Slim Framework (http://slimframework.com)
+ *
+ * @link      https://github.com/slimphp/Slim
+ * @copyright Copyright (c) 2016 Leonardo Neto
+ * @license   https://github.com/slimphp/Slim/blob/3.x/LICENSE.md (MIT License)
+ */
+
+namespace Aquino;
+
+
+class App
+{
+	
+	/**
+     * Current version
+     *
+     * @var string
+     */
+    const VERSION = '1.0.0';
+
+
+    public function __construct($container = [])
+    {
+
+    }
+}
